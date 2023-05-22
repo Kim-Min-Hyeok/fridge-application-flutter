183b65a0acb072496f761cd72cc475230ab606f7
Kim-Min-Hyeok <kkmin11203@gmail.com>
2023-05-22 09:41:48 +0900

    bottom navigation bar

--- my_fridge/lib/page/list.dart
+++ my_fridge/lib/page/list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_fridge/Theme/colorTheme.dart';
 
-
 class ListPage extends StatefulWidget {
   const ListPage({super.key});
 
@@ -24,18 +23,19 @@ class _ListPageState extends State<ListPage> {
             color: ColorStyle.primary,
           ),
         ),
-        title: const Text('상품목록',
-            style: TextStyle(
+        title: const Text(
+          '상품목록',
+          style: TextStyle(
               color: Color.fromRGBO(25, 63, 128, 100),
               fontSize: 20,
-              fontWeight: FontWeight.bold
-            ),
+              fontWeight: FontWeight.bold),
         ),
         actions: [
           Container(
             width: 80,
             child: Center(
-              child: Text('냉장고1',
+              child: Text(
+                '냉장고1',
                 style: TextStyle(
                   color: ColorStyle.secondary,
                   fontSize: 15,
@@ -45,20 +45,35 @@ class _ListPageState extends State<ListPage> {
           ), //나중에 저장된 냉장고 이름으로 바꿔야 함
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Image.asset("assets/images/list.png"),
-            activeIcon: Image.asset("assets/images/list_active.png"),
-            label: ''
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset("assets/images/recipe.png"),
-            activeIcon: Image.asset("assets/images/recipe.png"),
-            label: ''
-          ),
-        ]
-      ),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 100,
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {  },
+                    child: Image.asset(
+                      "assets/images/list_active.png",
+                      width: 80,
+                    ),
+                  ),
+                  const SizedBox(width: 100,),
+                  ElevatedButton(
+                    onPressed: () {  },
+                    child: Image.asset(
+                      "assets/images/recipe.png",
+                      width: 80,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          )
+        ],
+      )
     );
   }
 }
