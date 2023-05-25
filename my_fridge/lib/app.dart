55bd9407c1774cb5cd6e6489a5f691930eebef94
Kim-Min-Hyeok <kkmin11203@gmail.com>
2023-05-25 09:56:28 +0900

    splash

--- my_fridge/lib/app.dart
+++ my_fridge/lib/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:my_fridge/theme/colorTheme.dart';
+import 'package:page_transition/page_transition.dart';
 import 'page/list.dart';
 import 'page/home.dart';
 
@@ -11,9 +14,18 @@ class MyFridge extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyFridge',
-      initialRoute: '/',
+      //initialRoute: '/',
       routes: {
-        '/': (BuildContext context) => const HomePage(),
+        '/': (BuildContext context) => AnimatedSplashScreen(
+        duration: 2000,
+        splash: Image.asset('assets/images/splash.png'),
+        nextScreen: const HomePage(),
+        splashTransition: SplashTransition.fadeTransition,
+        pageTransitionType: PageTransitionType.fade,
+        splashIconSize: 300,
+        backgroundColor: ColorStyle.background,
+      ),
+        '/home': (BuildContext context) => const HomePage(),
         '/list': (BuildContext context) => const ListPage(),
       },
       theme: ThemeData(
