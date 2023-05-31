4be873bcabd990fff9bb231bb8fde3a74ff1461d
정성국 <ssoup0224@gmail.com>
2023-06-01 00:16:39 +0900

    Update add.dart

--- myfridge/lib/page/add.dart
+++ myfridge/lib/page/add.dart
@@ -5,13 +5,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:my_fridge/theme/colorTheme.dart';
 
 class AddPage extends StatefulWidget {
-  const AddPage({super.key});
+  const AddPage({super.key, this.restorationId});
+
+  final String? restorationId;
 
   @override
   State<AddPage> createState() => _AddPageState();
 }
 
-class _AddPageState extends State<AddPage> {
+class _AddPageState extends State<AddPage> with RestorationMixin {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final TextEditingController _productController = TextEditingController();
@@ -44,6 +46,56 @@ class _AddPageState extends State<AddPage> {
   }
 
   @override
+  String? get restorationId => widget.restorationId;
+
+  final RestorableDateTime _selectedDate = RestorableDateTime(DateTime.now());
+  late final RestorableRouteFuture<DateTime?> _restorableDatePickerRouteFuture =
+      RestorableRouteFuture<DateTime?>(
+    onComplete: _selectDate,
+    onPresent: (NavigatorState navigator, Object? arguments) {
+      return navigator.restorablePush(
+        _datePickerRoute,
+        arguments: _selectedDate.value.millisecondsSinceEpoch,
+      );
+    },
+  );
+
+  static Route<DateTime> _datePickerRoute(
+    BuildContext context,
+    Object? arguments,
+  ) {
+    return DialogRoute<DateTime>(
+      context: context,
+      builder: (BuildContext context) {
+        return DatePickerDialog(
+          restorationId: 'date_picker_dialog',
+          initialEntryMode: DatePickerEntryMode.calendar,
+          initialDate: DateTime.fromMillisecondsSinceEpoch(arguments! as int),
+          firstDate: DateTime(2023),
+          lastDate: DateTime(2030),
+        );
+      },
+    );
+  }
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_selectedDate, 'selected_date');
+    registerForRestoration(
+        _restorableDatePickerRouteFuture, 'date_picker_route_future');
+  }
+
+  void _selectDate(DateTime? newSelectedDate) {
+    if (newSelectedDate != null) {
+      setState(() {
+        _selectedDate.value = newSelectedDate;
+        _dateController.text =
+            '${_selectedDate.value.year}.${_selectedDate.value.month}.${_selectedDate.value.day}';
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -64,85 +116,100 @@ class _AddPageState extends State<AddPage> {
               fontWeight: FontWeight.bold),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(children: [
-          Row(
+      body: Column(children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Row(
             children: [
-              const Column(
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    '상품명:  ',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    '유통기한: ',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  SizedBox(
-                    width: 300,
-                    height: 40,
-                    child: TextField(
-                      controller: _productController,
-                      decoration: InputDecoration(
-                        fillColor: Colors.grey[200],
-                        filled: true,
-                      ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      left: 20.0,
+                      top: 20.0,
+                    ),
+                    child: Text(
+                      '상품명:  ',
+                      style: TextStyle(fontSize: 20),
                     ),
                   ),
                   const SizedBox(
                     height: 15,
                   ),
-                  SizedBox(
-                    width: 300,
-                    height: 40,
-                    child: TextField(
-                      controller: _dateController,
-                      decoration: InputDecoration(
-                        fillColor: Colors.grey[200],
-                        filled: true,
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: TextButton(
+                      child: const Text(
+                        '유통기한: ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        _restorableDatePickerRouteFuture.present();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 40,
+                      child: TextField(
+                        controller: _productController,
+                        decoration: InputDecoration(
+                          fillColor: Colors.grey[200],
+                          filled: true,
+                        ),
                       ),
                     ),
-                  )
-                ],
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SizedBox(
+                      width: 300,
+                      height: 40,
+                      child: TextField(
+                        controller: _dateController,
+                        decoration: InputDecoration(
+                          fillColor: Colors.grey[200],
+                          filled: true,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-              onPressed: () {
-                _addItem();
-                Navigator.pop(context);
-              },
-              style:
-                  ElevatedButton.styleFrom(backgroundColor: ColorStyle.primary),
-              child: const Text(
-                '저장',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              )),
-          const SizedBox(
-            height: 50,
-          ),
-          GestureDetector(
-            child: Container(
-              child: Image.asset('assets/images/barcode_scanner.png'),
-            ),
-            onTap: () {
-              Navigator.pushNamed(context, '/scan');
+        ),
+        const SizedBox(height: 30),
+        ElevatedButton(
+            onPressed: () {
+              _addItem();
+              Navigator.pop(context);
             },
-          )
-        ]),
-      ),
+            style:
+                ElevatedButton.styleFrom(backgroundColor: ColorStyle.primary),
+            child: const Text(
+              '저장',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            )),
+        const SizedBox(
+          height: 50,
+        ),
+        GestureDetector(
+          child: Container(
+            child: Image.asset('assets/images/barcode_scanner.png'),
+          ),
+          onTap: () {
+            Navigator.pushNamed(context, '/scan');
+          },
+        )
+      ]),
     );
   }
 }
