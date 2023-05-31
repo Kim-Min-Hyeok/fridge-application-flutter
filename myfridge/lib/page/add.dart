1c0da83e61e75e0fd83d724f6cb48b2e6660d465
정성국 <ssoup0224@gmail.com>
2023-05-31 17:35:09 +0900

    Update add.dart

--- myfridge/lib/page/add.dart
+++ myfridge/lib/page/add.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:my_fridge/theme/colorTheme.dart';
 
 class AddPage extends StatefulWidget {
@@ -9,6 +12,37 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final TextEditingController _productController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+
+  Future<void> _addItem() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      String userId = user.uid;
+
+      String product = _productController.text;
+      String date = _dateController.text;
+
+      try {
+        await _firestore.collection('items').add({
+          'userId': userId,
+          'product': product,
+          'date': date,
+        });
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Item added successfully'),
+        ));
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Failed to add item'),
+        ));
+        print('Error adding item: $e');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,15 +69,17 @@ class _AddPageState extends State<AddPage> {
         child: Column(children: [
           Row(
             children: [
-              Column(
+              const Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text(
+                  Text(
                     '상품명:  ',
                     style: TextStyle(fontSize: 20),
                   ),
-                  SizedBox(height: 15,),
-                  const Text(
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
                     '유통기한: ',
                     style: TextStyle(fontSize: 20),
                   ),
@@ -51,21 +87,25 @@ class _AddPageState extends State<AddPage> {
               ),
               Column(
                 children: [
-                  Container(
+                  SizedBox(
                     width: 300,
                     height: 40,
                     child: TextField(
+                      controller: _productController,
                       decoration: InputDecoration(
                         fillColor: Colors.grey[200],
                         filled: true,
                       ),
                     ),
                   ),
-                  SizedBox(height: 15,),
-                  Container(
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
                     width: 300,
                     height: 40,
                     child: TextField(
+                      controller: _dateController,
                       decoration: InputDecoration(
                         fillColor: Colors.grey[200],
                         filled: true,
@@ -76,27 +116,28 @@ class _AddPageState extends State<AddPage> {
               )
             ],
           ),
-          const SizedBox(height:30),
+          const SizedBox(height: 30),
           ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            }, 
-            child: Text('저장',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: ColorStyle.primary
-            )
+              onPressed: () {
+                _addItem();
+                Navigator.pop(context);
+              },
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: ColorStyle.primary),
+              child: const Text(
+                '저장',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              )),
+          const SizedBox(
+            height: 50,
           ),
-          const SizedBox(height: 50,),
           GestureDetector(
             child: Container(
               child: Image.asset('assets/images/barcode_scanner.png'),
             ),
-            onTap: () {
-            },
+            onTap: () {},
           )
         ]),
       ),
