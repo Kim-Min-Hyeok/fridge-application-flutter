47ca3c40cabfd07ddf9ca7b34522037a1250d2c6
정성국 <ssoup0224@gmail.com>
2023-05-31 21:31:56 +0900

    Create product.dart

---
+++ myfridge/lib/model/product.dart
@@ -0,0 +1,11 @@
+class Product {
+  final String userUid;
+  final String product;
+  final String date;
+
+  Product({
+    required this.userUid,
+    required this.product,
+    required this.date,
+  });
+}
