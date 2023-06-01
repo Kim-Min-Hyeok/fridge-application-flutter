7def720e6cd3075a44a565aa0151788aeb9c0a0d
정성국 <ssoup0224@gmail.com>
2023-06-01 14:59:32 +0900

    Update product.dart

--- myfridge/lib/model/product.dart
+++ myfridge/lib/model/product.dart
@@ -2,10 +2,14 @@ class Product {
   final String userUid;
   final String product;
   final String date;
+  final String documentId;
+  final int difference;
 
   Product({
     required this.userUid,
     required this.product,
     required this.date,
+    required this.documentId,
+    required this.difference,
   });
 }
